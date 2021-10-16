6009c1db089e9544fbaecb7034291e3d41db4f39
Nishit singh <djnishu6@gmail.com>
2021-10-16 15:59:32 +0530

    two

--- routes/route.js
+++ routes/route.js
@@ -11,6 +11,15 @@ const {authRole}  = require('../middlewares/authRole');
 
 // Getting all the products
 
+router.get('/',(req,res)=>{
+
+    res.render('products/home')
+})
+
+
+
+
+
 router.get('/products',async(req,res)=>{
 
    try {
